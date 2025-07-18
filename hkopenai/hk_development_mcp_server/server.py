d5715978d3f51e36390ea12712989fb509ac9a9f
Neo Alienson <neo@01man.com>
2025-07-18 21:01:51 +0800

    refactor

--- hkopenai/hk_development_mcp_server/server.py
+++ hkopenai/hk_development_mcp_server/server.py
@@ -4,13 +4,13 @@ It is responsible for creating and configuring the server, and registering the a
 """
 
 from fastmcp import FastMCP
-import hkopenai.hk_development_mcp_server.tools.new_building_plan_processed
+from .tools import new_building_plan_processed
 
 
 def server():
     """Create and configure the MCP server"""
     mcp = FastMCP(name="HK OpenAI development Server")
 
-    hkopenai.hk_development_mcp_server.tools.new_building_plan_processed.register(mcp)
+    new_building_plan_processed.register(mcp)
 
     return mcp
